521b40129bc25dc1370e7c1062e1f5fa26fa5529
fatima13kh <fatima495khamis@gmail.com>
2025-09-21 19:32:37 +0300

    building foods controller

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const passUserToView = require("./middleware/pass-user-to-view.js");
 
 // Controllers
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -52,7 +53,9 @@ app.get('/', (req, res) => {
 
 app.use('/auth', authController);
 
+
 // PROTECTED
+app.use('/users/:userId/foods', foodsController);
 
 app.get("/vip-lounge", isSignedIn, (req, res) => {
     res.send(`Welcome to the party ${req.session.user.username}.`);
